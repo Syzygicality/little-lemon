4fcf24b5b5a39561e92eb7af7479a7b64d9479a9
Edison Wang <ew040607@gmail.com>
2025-08-14 18:59:28 +0000

    created menu, booking, and user views

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import Menu, Booking
+from django.contrib.auth.models import User
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+from rest_framework import generics, viewsets, permissions
 
-# Create your views here.
+# Create your views here. 
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated]
+
